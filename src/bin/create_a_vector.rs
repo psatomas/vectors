19ec6dabd3cfb4fe3dc58ec7f23cf28208d8fbc7
psatomas <psatomas@gmail.com>
2025-07-26 05:58:06 -0300

    feat: add create_a_vector.rs

---
+++ src/bin/create_a_vector.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let pizza_diameters: Vec<i8> = vec![8, 10, 12, 14];
+    println!("{pizza_diameters:?}");
+
+    let pastas= vec!["Rigatoni", "Angel hain", "Fettucine"];
+    println!("{pastas:?}");
+
+
+}
